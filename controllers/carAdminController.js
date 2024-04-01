4ff91042860a0ac26476d6652c2ec395da786ef8
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-04-02 03:10:34 +0700

    add filter and upload image use multer

--- controllers/carAdminController.js
+++ controllers/carAdminController.js
@@ -1,8 +1,32 @@
 const { Car } = require("../models");
+const { Op } = require('sequelize');
+const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const { promisify } = require('util');
 
 const carPage = async (req, res) => {
     try {
-        const cars = await Car.findAll();
+        let cars;
+        if (req.query.category && req.query.category === 'small') {
+            // Filter mobil dengan kategori "small"
+            cars = await Car.findAll({ where: { category: 'Small' || "small" } });
+        }
+        else if (req.query.category && req.query.category === "medium") {
+            // Filter mobil dengan kategori "medium"
+            cars = await Car.findAll({ where: { category: "Medium" || "medium" } });
+        }
+        else if (req.query.category && req.query.category === "large") {
+            // Filter mobil dengan kategori "lare"
+            cars = await Car.findAll({ where: { category: "Large" || "large" } });
+        }
+        //Filter seacrh untuk nama mobil
+        else if (req.query.searchName) {
+            const searchName = req.query.searchName.toLowerCase(); // Ubah input menjadi huruf kecil
+            cars = await Car.findAll({ where: { name: { [Op.iLike]: `%${searchName}%` } } });
+        } else {
+            cars = await Car.findAll();
+        }
 
         res.render("cars/index.ejs", {
             cars,
@@ -25,15 +49,47 @@ const createCarPage = async (req, res) => {
     }
 };
 
+// Konfigurasi Multer untuk menyimpan gambar di public/images
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const dir = `public/images`;
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+        cb(null, dir);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+// Upload dengan Multer
+const upload = multer({ storage: storage }).single('image');
+
 const createCar = async (req, res) => {
     try {
-        await Car.create(req.body);
-        req.flash("message", "Ditambah");
-        res.redirect("/cars");
+        // Upload gambar dengan Multer
+        upload(req, res, async function (err) {
+            if (err instanceof multer.MulterError) {
+                // Aksi jika terjadi kesalahan saat upload
+                console.log(err.message);
+            } else if (err) {
+                // Aksi jika terjadi kesalahan lain saat upload
+                console.log(err.message);
+            } else {
+                // Simpan data mobil ke dalam basis data
+                const { name, price, category } = req.body;
+                const imageUrl = req.file ? '/images/' + req.file.filename : ''; // Path gambar jika ada
+                await Car.create({ name, price, category, imageUrl });
+                req.flash("message", "Ditambah");
+                res.redirect("/cars");
+            }
+        });
     } catch (err) {
         console.log(err.message);
     }
 };
+
 
 const editCarPage = async (req, res) => {
     try {
--- routes/carAdminRouter.js
+++ routes/carAdminRouter.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
+const express = require("express");
 
 const CarAdmin = require("../controllers/carAdminController");
 
 router.get("/", CarAdmin.carPage);
 router.get("/create", CarAdmin.createCarPage);
-router.post("/admin/create", CarAdmin.createCar);
 router.get("/edit/:id", CarAdmin.editCarPage);
+
+router.post("/admin/create", CarAdmin.createCar);
 router.post("/admin/edit/:id", CarAdmin.editCar);
 router.post("/admin/delete/:id", CarAdmin.deleteCar);
 

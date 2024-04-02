d3f9697358a9bc9efcaf4a0b0159c0767cb10ec1
rayhanhafa <115576133+rayhanhafa@users.noreply.github.com>
2024-04-02 14:26:45 +0700

    add api backend postman

--- controllers/carController.js
+++ controllers/carController.js
@@ -1,10 +1,62 @@
 const { Car } = require("../models");
 
+const getAllCars = async (req, res) => {
+    try {
+        // Mengambil semua data mobil dari basis data
+        const cars = await Car.findAll();
+
+        // Mengirim response dengan data mobil yang berhasil diambil
+        res.status(200).json({
+            status: "success",
+            data: {
+                cars
+            }
+        });
+    } catch (err) {
+        // Mengirim response dengan pesan error jika terjadi kesalahan
+        res.status(500).json({
+            status: "error",
+            message: err.message
+        });
+    }
+};
+
+const getCarById = async (req, res) => {
+    const { id } = req.params; // Mendapatkan _id dari parameter URL
+
+    try {
+        // Mengambil data mobil berdasarkan _id dari basis data
+        const car = await Car.findByPk(id);
+
+        // Jika data mobil ditemukan, kirim response dengan data mobil
+        if (car) {
+            res.status(200).json({
+                status: "success",
+                data: {
+                    car
+                }
+            });
+        } else {
+            // Jika data mobil tidak ditemukan, kirim response dengan pesan bahwa data tidak ditemukan
+            res.status(404).json({
+                status: "error",
+                message: "Data car not found"
+            });
+        }
+    } catch (err) {
+        // Mengirim response dengan pesan error jika terjadi kesalahan
+        res.status(500).json({
+            status: "error",
+            message: err.message
+        });
+    }
+};
+
 const createCar = async (req, res) => {
-    //destructuting object
     const { name, category, price, image } = req.body
 
     try {
+        //create data baru kedalam database
         const newCar = await Car.create({
             name,
             category,
@@ -12,6 +64,7 @@ const createCar = async (req, res) => {
             image,
         })
 
+        // Mengirim response dengan data mobil yang berhasil diambil
         res.status(200).json({
             status: "succes",
             data: {
@@ -19,11 +72,17 @@ const createCar = async (req, res) => {
             }
         })
     } catch (err) {
-        console.log(err.message)
+        // Mengirim response dengan pesan error jika terjadi kesalahan
+        res.status(500).json({
+            status: "error",
+            message: err.message
+        });
     }
 }
 
 
 module.exports = {
+    getAllCars,
+    getCarById,
     createCar,
 }
--- routes/carRouter.js
+++ routes/carRouter.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 
 const Car = require("../controllers/carController");
 
+router.get("/", Car.getAllCars);
+router.get("/:id", Car.getCarById);
 router.post("/", Car.createCar);
+
 
 module.exports = router;
